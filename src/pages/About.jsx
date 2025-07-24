8aeed82677dabdd27b918fcf9bb6309fc86c7edb
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-24 18:44:28 +0700

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="page about">
+      <h1>Tentang Saya</h1>
+      <p>Saya seorang developer web. Membuat website profesional dengan React.</p>
+    </div>
+  );
+}
