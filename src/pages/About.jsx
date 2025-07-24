202d44fb250a873220777a5dd7d806db4117fb33
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-24 21:19:00 +0700

    Delete src/pages directory

--- src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="page about">
-      <h1>Tentang Saya</h1>
-      <p>Saya seorang developer web. Membuat website profesional dengan React.</p>
-    </div>
-  );
-}
