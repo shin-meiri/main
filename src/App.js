53c75b655d09760cb9def92065f682a1caedcef3
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-24 19:02:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Post from './pages/Post';
 import About from './pages/About';
