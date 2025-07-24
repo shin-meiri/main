316c8dc935a374b7d347c6782063dc8ba6a800bb
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 00:20:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import AdminLayout from './pages/Admin/AdminLayout';
+import Dashboard from './pages/Admin/Dashboard';
 
 const App = () => {
   return (
-    <div style={styles.app}>
-      <Header />
-      <main style={styles.main}>
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div style={styles.app}>
+        <Header />
+        <main style={styles.main}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<Dashboard />} /> {/* /admin -> dashboard */}
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
