ee5336178850718e6433dbd031cabae16a47657a
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-24 23:25:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+//import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
