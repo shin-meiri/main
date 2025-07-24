e7989d4b6771ed0a2bbaf3e3642c4cd11fbb5b50
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-24 22:06:13 +0700

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import userData from '../../data/user.json';
+import './Login.css';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === userData.username && password === userData.password) {
+      onLogin(); // simpan status login
+    } else {
+      setError('Username atau password salah!');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h2>Admin Login</h2>
+        {error && <p className="error">{error}</p>}
+        <div>
+          <label>Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
