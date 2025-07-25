070cc3f9f3897146dc04c3dd1273af3549b4b3ec
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 19:07:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-// Di App.js
+// App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Admin from './src/Admin';
+
 function App() {
   const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/admin/*"
-        element={isLoggedIn ? <Admin /> : <Navigate to="/" />}
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/admin/*"
+          element={isLoggedIn ? <Admin /> : <Navigate to="/" replace />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
