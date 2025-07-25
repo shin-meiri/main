d4558e8734077f1d2a3a6e765916f02a7662a5db
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 14:50:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // App.js
 import React from 'react';
-import TestConnection from './TestConnection'; // Pastikan nama file sama
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import DynamicPage from './DynamicPage';
 
 function App() {
   return (
-    <div className="App">
-      <TestConnection />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/page/:slug" element={<DynamicPage />} />
+          <Route path="/" element={<HomeRedirect />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function HomeRedirect() {
+  // Redirect ke /page/home
+  useEffect(() => {
+    window.location.href = '/page/home';
+  }, []);
+  return <div>Memuat...</div>;
 }
 
 export default App;
