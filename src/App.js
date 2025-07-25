c65118a4b1b21734bf9d61c9c0fe2532ec00c715
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 16:37:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
+import DynamicPage from './DynamicPage';
 import AdminPanel from './AdminPanel';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/page/:slug" element={<DynamicPage />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/" element={<Navigate to="/page/home" />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
+// Tambahkan Navigate
+import { Navigate } from 'react-router-dom';
 export default App;
