d7f6f2956e54cc677f60a83173fc31a8d2e8ebc4
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 21:08:01 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     setError('');
     
     try {
-      const res = await fetch('http://localhost/api/datadb.php', {
+      const res = await fetch('/api/datadb.php', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
