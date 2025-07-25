dfd772dc754c9de3e38e7258bdab3f9a3f07d5b5
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 14:35:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TestConnection from './TestConnection'; // Pastikan path benar
+import TestConnection from './TestConnection'; // Pastikan nama file sama
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/test-db" element={<TestConnection />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <TestConnection />
+    </div>
   );
 }
 
