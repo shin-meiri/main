d5f2d7ec752bbbd358ae24825589c537ff70e087
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 12:44:08 +0700

    Delete src/components directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return `
-    <footer style="padding: 1rem; background: #333; color: white; text-align: center;">
-      &copy; 2025 My React App
-    </footer>
-  `;
-}
