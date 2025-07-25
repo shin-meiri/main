8f1d65c874d5fe984da200594156f0a56435abdf
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-25 14:01:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TestConnection from './TestConnection';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
-          <Link to="/test">Tes Koneksi DB</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/test" element={<TestConnection />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/test-db" element={<TestConnection />} />
+        {/* Rute lain... */}
+      </Routes>
     </Router>
   );
 }
