3621487b7100b2ba08f83f9a326710814bbd9f2e
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-27 01:12:42 +0700

    Update DatabaseConnector.jsx

--- src/components/DatabaseConnector.jsx
+++ src/components/DatabaseConnector.jsx
@@ -28,7 +28,7 @@ const DatabaseConnector = () => {
   const testConnection = async () => {
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:8000/backend/api/konek.php', {
+      const response = await fetch('https://bos.free.nf/api/konek.php', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
