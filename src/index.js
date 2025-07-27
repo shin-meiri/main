a933ff1038e7e42915fefa89e684a150730789ad
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-27 15:33:16 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-// frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Jika ada file CSS tambahan
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
