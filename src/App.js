d702b96776623bd970fa46aa8e7b2ca0aa7eb61d
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-28 00:47:32 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
+// App.js
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Pages from './pages/Pages';
+import Pages from './Pages';
+import './App.css';
 
 function App() {
-  const isAuthenticated = localStorage.getItem('isAuthenticated');
-
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="login" />} />
-          <Route path="/login" element={<Login />} />
-          <Route 
-            path="/pages" 
-            element={isAuthenticated ? <Pages /> : <Navigate to="login" />} 
-          />
-          <Route path="*" element={<Navigate to="login" />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Pages />
+    </div>
   );
 }
 
