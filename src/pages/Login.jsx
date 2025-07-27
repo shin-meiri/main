5ef03ed66fc213c127e69d44d31fa8829f21d60f
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-27 23:21:27 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -75,7 +75,7 @@ const Login = () => {
 
     try {
       // Membaca file room.json
-      const response = await axios.get('/room.json');
+      const response = await axios.get('/api/rom.json');
       const { user, pass } = response.data;
 
       // Validasi kredensial
