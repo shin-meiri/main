1dfdedc60d02e67d4ef1bae022ee21d1d1b0498d
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-28 17:38:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pages from './pages/Pages';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Pages />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
