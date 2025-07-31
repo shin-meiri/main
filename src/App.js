3b4a0a164077037eb69d363281280f9a5f03a0f1
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 10:49:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </Router>
   );
