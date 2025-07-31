a4ff399b886c4c610dbd366dd8aea9d6017760f4
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 22:35:58 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
