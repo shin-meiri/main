0cef90e26dc0fe76cdf0a201ece3f4b883ef4f3c
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 10:15:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
 function App() {
   return (
- <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-   </Router>
+    </Router>
   );
 }
 
