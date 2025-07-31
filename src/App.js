67787976506e1758c410981027308903bc64f2ed
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 10:10:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 
