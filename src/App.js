5e781823bb4d27e98d97580bcc655e78bc65a8d1
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 10:53:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route
-          path="/dashboard"
+          path="/"
           element={
             <PrivateRoute>
               <Dashboard />
