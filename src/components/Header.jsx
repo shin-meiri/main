373a271b376fc81453af5ce6b735fc25adbee6fc
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 15:46:09 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,7 @@
-// Header.jsx
 import React from 'react';
 
 const Header = () => {
-  return (
-    <header style={{ backgroundColor: '#333', color: '#fff', padding: '1rem', textAlign: 'center' }}>
-      <h1>My App Header</h1>
-    </header>
-  );
+  return <></>;
 };
 
 export default Header;
