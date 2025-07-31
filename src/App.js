b54258838acabee30b8d667702f35873a377391c
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 10:11:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Dashboard from './components/Dashboard';
 
 function App() {
   return (
- 
+ <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-   
+   </Router>
   );
 }
 
