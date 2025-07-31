e33c7c560eab87544ae64c1bbce48b33b661f724
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-07-31 20:51:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Pages from './pages/Pages';
