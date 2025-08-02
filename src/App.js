bca7ca35796fc061ec00044d2886aa12f34ade40
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-08-02 23:12:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+// src/App.js
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import ProtectedRoute from './components/ProtectedRoute';
-import './styles.css';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    sessionStorage.setItem('isLoggedIn', 'true');
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    if (sessionStorage.getItem('isLoggedIn')) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <Router>
-      <div className="app">
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={<Login onLogin={handleLogin} />}
+        />
+      </Routes>
     </Router>
   );
 }
