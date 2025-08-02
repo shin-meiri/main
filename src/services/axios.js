d54d9ba75f92ddb9dc6b31baf1b3028f5aa1e2e3
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-08-02 14:26:29 +0700

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost/api', // Sesuaikan dengan lokasi PHP
+  baseURL: 'http://bos.free.nf/api', // Sesuaikan dengan lokasi PHP
   withCredentials: true, // Jika pakai session PHP
 });
 
