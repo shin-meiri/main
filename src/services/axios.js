63ae1130771129f769c0ad08eb3633421cb1dc0f
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-08-02 22:48:50 +0700

    Delete src/services directory

--- src/services/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://bos.free.nf/api', // Sesuaikan dengan lokasi PHP
-  withCredentials: true, // Jika pakai session PHP
-});
-
-export default api;
