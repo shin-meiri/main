3c6b2d717e8ccb0e22a115b6fd78897e55384b13
shin-meiri <78695422+shin-meiri@users.noreply.github.com>
2025-08-02 11:46:11 +0700

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      {/* Form login atau konten lainnya akan ditambahkan di sini */}
+    </div>
+  );
+};
+
+export default Login;
